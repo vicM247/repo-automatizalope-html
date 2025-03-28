bc3a835ba5ab2cd931f3af8c3c0fa156b0913f7b
vicM247 <victor.vasquez@marcadores247.com>
2025-03-28 16:18:46 -0500

    Actualizaciones editor Netlify

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -2,7 +2,7 @@ import { defineStackbitConfig, SiteMapEntry } from "@stackbit/types";
 import { GitContentSource } from "@stackbit/cms-git";
 
 export default defineStackbitConfig({
-  stackbitVersion: "~0.6.0",
+  stackbitVersion: "~2.1.14",
   contentSources: [
     new GitContentSource({
       rootPath: __dirname,
